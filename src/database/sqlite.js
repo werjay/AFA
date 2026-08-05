3f6afd44d924589efb461b87796c10be48e2e87b
neo <neo.chang@cctch.com>
2026-08-05 14:51:46 +0800

    fix: create database directory automatically

--- src/database/sqlite.js
+++ src/database/sqlite.js
@@ -2,9 +2,9 @@ const Database = require("better-sqlite3");
 const path = require("path");
 const fs = require("fs");
 
-const dbPath = path.join(
+const dataDir = path.join(
     __dirname,
-    "../../data/afa.db"
+    "../../data"
 );
 
 if (!fs.existsSync(dataDir)) {
